fd7b9e044d0c538b2e933622e347f6969a2996b2
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2020-12-26 17:30:39 +0100

    Add state and check if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React from 'react';
+//import { Route, Switch, Redirect } from 'react-router-dom';
+import Login from './components/Login/Login';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          welcome to elms
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    loggedIn: false
+  }
+  render () {
+    if (this.state.loggedIn) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              welcome to elms
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      );
+    }
+    return <Login />
+  }
+  
 }
 
 export default App;
