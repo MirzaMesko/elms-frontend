f6417975a4b1cc6f8397e37442f7b9404270a441
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2020-12-26 17:27:11 +0100

    Add login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+    render () {
+        return (
+            <div>
+                <p>Please log in to continue!</p>
+            </div>
+        )
+    }
+}
+
+export default Login;
