3c629990f59490c1067513a14d31cc8d005c3f04
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2020-12-26 18:37:47 +0100

    Add state and login functionality

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const LoginForm = styled.div`
+    margin: 200px auto;
+    text-align: center;
+    width: 300px;
+    border: 2px solid rgb(84, 163, 233);
+    box-shadow: 10px 10px 15px #aaaaaa;
+`;
+
+const Field = styled.input`
+    display: block;
+    margin: 20px auto;
+    width: 200px;
+    height: 30px;
+`;
+
+const LoginConfirm = styled.button`
+    display: block;
+    margin: 20px auto;
+    width: 100px;
+    height: 30px;
+`;
+
+
 
 class Login extends React.Component {
-    render () {
+    state = {
+        username: '',
+        password: '',
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({ username: event.target.value})
+    }
+
+    handlePasswordChange = (event) => {
+        this.setState({ password: event.target.value})
+    }
+
+    login = () => {
+        const { username, password } = this.state;
+        this.props.onLogin(username, password)
+    }
+
+    render() {
         return (
-            <div>
+            <LoginForm>
                 <p>Please log in to continue!</p>
-            </div>
+                <Field placeholder='username' type='text' onChange={this.handleUsernameChange} value={this.state.username} />
+                <Field placeholder='password' type='password' onChange={this.handlePasswordChange} value={this.state.password} />
+                <LoginConfirm onClick={this.login} >Log in</LoginConfirm>
+            </LoginForm>
         )
     }
 }
