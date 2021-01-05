5c1adc22f3b818b69c9f9134677b3e3b1f63e6a9
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-01-05 19:16:02 +0100

    ELMS-5 add routes for redirecting guests to loggin

--- src/App.js
+++ src/App.js
@@ -2,43 +2,46 @@ import { history as historyPropTypes } from 'history-prop-types';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
-import GuardedRoute from './components/GuardedRoute';
+import ManageUsers from './components/ManageUsers';
 import { logout } from './actions/users';
 import Login from './components/Login';
 
 function App(props) {
   const { loggedIn, history, error, authUser, onLogout } = props;
-  // eslint-disable-next-line
-  console.log(history, loggedIn, error);
-  return (
+
+  let routes = (
     <Switch>
-      <Route path="/login" render={() => <Login error={error} history={history} />} />
-      <GuardedRoute
-        path="/"
-        exact
-        component={Dashboard}
-        auth={loggedIn}
-        props={{ user: authUser, onLogout }}
-      />
+      <Route path="/login" render={() => <Login error={error} history={history} />} />;
+      <Redirect to="/login" />
     </Switch>
   );
+
+  if (loggedIn) {
+    routes = (
+      <Dashboard user={authUser} onLogout={onLogout} history={history}>
+        <Switch>
+          <Route path="/manage/users" render={() => <ManageUsers history={history} />} />
+        </Switch>
+      </Dashboard>
+    );
+  }
+
+  return routes;
 }
 
 App.propTypes = {
   history: PropTypes.shape(historyPropTypes).isRequired,
   loggedIn: PropTypes.bool.isRequired,
-  error: PropTypes.string.isRequired,
-  authUser: PropTypes.string.isRequired,
+  error: PropTypes.bool.isRequired,
+  authUser: PropTypes.string,
   onLogout: PropTypes.func.isRequired,
 };
 // eslint-disable-next-line
 const mapStateToProps = (state) => {
-  // eslint-disable-next-line
   return {
-    // eslint-disable-next-line
     loggedIn: state.loggedIn,
     error: state.error,
     authUser: state.authUser.username,
@@ -46,9 +49,7 @@ const mapStateToProps = (state) => {
 };
 // eslint-disable-next-line
 const mapDispatchToProps = (dispatch) => {
-  // eslint-disable-next-line
   return {
-    // eslint-disable-next-line
     onLogout: () => dispatch(logout()),
   };
 };
