1d3387eb20ef28a4ee497f8731e15ad0ea3cd4ba
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-01-08 19:08:11 +0100

    ELMS-3 add functionality to show link is selected

--- src/components/listItems.js
+++ src/components/listItems.js
@@ -1,19 +1,49 @@
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
 import GroupIcon from '@material-ui/icons/Group';
+import { history as historyPropTypes } from 'history-prop-types';
+import PropTypes from 'prop-types';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-const mainListItems = (
-  <div>
-    <ListItem button component={Link} to="/manage/users">
-      <ListItemIcon>
-        <GroupIcon />
-      </ListItemIcon>
-      <ListItemText primary="Manage Users" />
-    </ListItem>
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
-export default mainListItems;
+const MainListItems = (props) => {
+  const { show, history } = props;
+
+  const classes = useStyles();
+
+  const display = show ? 'flex' : 'none';
+  return (
+    <div style={{ display }}>
+      <div className={classes.root}>
+        <ListItem
+          button
+          component={Link}
+          to="/manage/users"
+          selected={history.location.pathname === '/manage/users'}
+        >
+          <ListItemIcon>
+            <GroupIcon />
+          </ListItemIcon>
+          <ListItemText primary="Manage Users" />
+        </ListItem>
+      </div>
+    </div>
+  );
+};
+
+MainListItems.propTypes = {
+  show: PropTypes.bool.isRequired,
+  history: PropTypes.shape(historyPropTypes).isRequired,
+};
+
+export default withRouter(MainListItems);
