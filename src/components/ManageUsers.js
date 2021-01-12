da1bc3f7e752598ff1acfd98d98c09b20246d283
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-01-12 17:31:48 +0100

    ELMS-1 move CustomizedSnackbar from FormDialogue

--- src/components/ManageUsers.js
+++ src/components/ManageUsers.js
@@ -1,9 +1,13 @@
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import React from 'react';
+import CustomizedSnackbars from './Snackbar';
 import FormDialogue from './FormDialogue';
 import Table from './Table';
+import { getUsers } from '../actions/users';
 
 const useStyles = makeStyles((theme) => ({
   submit: {
@@ -11,9 +15,13 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ManageUsers() {
+function ManageUsers(props) {
+  const { token, onGetUsers, users } = props;
   const classes = useStyles();
   const [openDialogue, setOpenDialogue] = React.useState(false);
+  const [severity, setSeverity] = React.useState('');
+  const [openSnackbar, setOpenSnackbar] = React.useState(false);
+  const [newUser, setNewUser] = React.useState('');
 
   const handleOpen = () => {
     setOpenDialogue(true);
@@ -23,8 +31,22 @@ export default function ManageUsers() {
     setOpenDialogue(false);
   };
 
+  const showSnackbar = (show, status, message) => {
+    setOpenSnackbar(show);
+    setNewUser(message);
+    setSeverity(status);
+    setTimeout(() => {
+      setOpenSnackbar(false);
+    }, 6000);
+  };
+
+  React.useEffect(() => {
+    onGetUsers(token, { offset: 0 });
+  }, [token]);
+
   return (
     <Box>
+      <CustomizedSnackbars show={openSnackbar} severity={severity} message={newUser} />
       <Button
         type="submit"
         variant="contained"
@@ -34,8 +56,30 @@ export default function ManageUsers() {
       >
         New User
       </Button>
-      <Table />
-      <FormDialogue show={openDialogue} close={handleClose} />
+      <Table users={users} />
+      <FormDialogue
+        show={openDialogue}
+        close={handleClose}
+        onGetUsers={onGetUsers}
+        onShowSnackbar={showSnackbar}
+      />
     </Box>
   );
 }
+
+ManageUsers.propTypes = {
+  token: PropTypes.string.isRequired,
+  onGetUsers: PropTypes.func.isRequired,
+  users: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  token: state.authUser.token,
+  users: state.users,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onGetUsers: (token, params) => dispatch(getUsers(token, params)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageUsers);
