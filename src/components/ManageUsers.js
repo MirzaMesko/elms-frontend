1586a08769b9cd00dae47ebcc72aa6139d37b6c1
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-02-23 22:00:36 +0100

    replace default html form with material UI form

--- src/components/ManageUsers.js
+++ src/components/ManageUsers.js
@@ -1,6 +1,13 @@
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, styled } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
+import SearchIcon from '@material-ui/icons/Search';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -9,6 +16,49 @@ import CustomizedSnackbars from './Snackbar';
 import FormDialogue from './FormDialogue';
 import UserTable from './UserTable';
 import { getUsers } from '../actions/users';
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  'label + &': {
+    margin: theme.spacing(3, 2, 2, 0),
+  },
+  '& .MuiInputBase-input': {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 46px 10px 32px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(3, 2, 2, 1),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
 
 const useStyles = makeStyles((theme) => ({
   submit: {
@@ -49,9 +99,9 @@ function ManageUsers(props) {
   const [searchResults, setSearchResults] = React.useState([]);
   const [search, setSearch] = React.useState('');
   const [filter, setFilter] = React.useState('username');
-  const [roleFilter, setRoleFilter] = React.useState('');
+  const [roleFilter, setRoleFilter] = React.useState('All');
 
-  const isAdmin = Object.values(roles).includes('Admin');
+  const isAdmin = roles.includes('Admin');
 
   const handleOpen = () => {
     setOpenDialogue(true);
@@ -115,49 +165,47 @@ function ManageUsers(props) {
             New User
           </Button>
         )}
-        <form className={classes.search} onSubmit={(e) => e.preventDefault()}>
-          <label htmlFor="search" className={classes.label}>
-            <input
-              id="search"
-              type="text"
-              placeholder=""
-              value={search}
-              className={classes.input}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            Search users by
-          </label>
-        </form>
-        <form className={classes.search} onSubmit={(e) => e.preventDefault()}>
-          <label htmlFor="search" className={classes.label}>
-            <select
-              className={classes.option}
-              selected={filter}
-              onChange={(e) => setFilter(e.target.value)}
-            >
-              <option value="username">username</option>
-              <option value="email">email</option>
-              <option value="name">name</option>
-            </select>
-          </label>
-        </form>
-        <div style={{ marginLeft: '4rem' }}>
-          <form className={classes.search} onSubmit={(e) => e.preventDefault()}>
-            <label htmlFor="search" className={classes.label}>
-              Showing
-              <select
-                className={classes.option}
-                selected={roleFilter}
-                onChange={(e) => setRoleFilter(e.target.value)}
-              >
-                <option value="All">all users</option>
-                <option value="Admin">Admins</option>
-                <option value="Member">Members</option>
-                <option value="Librarian">Librarians</option>
-              </select>
-            </label>
-          </form>
-        </div>
+        <FormControl variant="standard">
+          <InputLabel htmlFor="demo-customized-textbox">search users</InputLabel>
+          <BootstrapInput
+            id="demo-customized-textbox"
+            type="text"
+            placeholder="Search…"
+            value={search}
+            label="search users"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <SearchIconWrapper>
+            <SearchIcon color="primary" />
+          </SearchIconWrapper>
+        </FormControl>
+        <Typography style={{ margin: '2rem 0.5rem' }}>by</Typography>
+        <FormControl variant="standard">
+          <InputLabel htmlFor="demo-customized-textbox"> </InputLabel>
+          <Select
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            input={<BootstrapInput />}
+          >
+            <MenuItem value="username">username</MenuItem>
+            <MenuItem value="email">email</MenuItem>
+            <MenuItem value="name">name</MenuItem>
+          </Select>
+        </FormControl>
+        <Typography style={{ margin: '2rem 0.5rem 2rem 9rem' }}>showing</Typography>
+        <FormControl variant="standard">
+          <InputLabel htmlFor="demo-customized-textbox"> </InputLabel>
+          <Select
+            value={roleFilter}
+            onChange={(e) => setRoleFilter(e.target.value)}
+            input={<BootstrapInput />}
+          >
+            <MenuItem value="All">all users</MenuItem>
+            <MenuItem value="Admin">Admins</MenuItem>
+            <MenuItem value="Member">Members</MenuItem>
+            <MenuItem value="Librarian">Librarians</MenuItem>
+          </Select>
+        </FormControl>
       </div>
 
       <UserTable users={searchResults} onShowSnackbar={showSnackbar} />
@@ -175,7 +223,7 @@ ManageUsers.propTypes = {
   token: PropTypes.string.isRequired,
   onGetUsers: PropTypes.func.isRequired,
   users: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-  roles: PropTypes.objectOf(PropTypes.string).isRequired,
+  roles: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 const mapStateToProps = (state) => ({
