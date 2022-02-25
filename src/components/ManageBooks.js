5b25fbf1245d0334d5a3c07ea5d30ef64d35739d
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-02-25 12:29:12 +0100

    replace default html form with material UI form

--- src/components/ManageBooks.js
+++ src/components/ManageBooks.js
@@ -1,6 +1,13 @@
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, styled } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
+import SearchIcon from '@material-ui/icons/Search';
 import { connect } from 'react-redux';
 import { history as historyPropTypes } from 'history-prop-types';
 import PropTypes from 'prop-types';
@@ -10,6 +17,49 @@ import CustomizedSnackbars from './Snackbar';
 import FormDialogue from './FormDialogue';
 import BookTable from './BookTable';
 import { getBooks } from '../actions/books';
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  'label + &': {
+    margin: theme.spacing(3, 2, 2, 0),
+  },
+  '& .MuiInputBase-input': {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 46px 10px 32px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(3, 2, 2, 1),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
 
 const useStyles = makeStyles((theme) => ({
   submit: {
@@ -52,11 +102,11 @@ function ManageBooks(props) {
   const [categorySearchResults, setCategorySearchResults] = React.useState([]);
   const [search, setSearch] = React.useState('');
   const [searchFilter, setSearchFilter] = React.useState('title');
-  const [roleFilter, setRoleFilter] = React.useState('');
+  const [roleFilter, setRoleFilter] = React.useState('all books');
 
   const isAdmin = roles.includes('Admin');
   const categories = [
-    'All books',
+    'all books',
     'Politics',
     'History',
     'Romance',
@@ -92,7 +142,7 @@ function ManageBooks(props) {
   }, [books]);
 
   const filterByCategorie = () => {
-    if (roleFilter === 'All books') {
+    if (roleFilter === 'all books') {
       setCategorySearchResults(books);
       return;
     }
@@ -135,53 +185,51 @@ function ManageBooks(props) {
             New Book
           </Button>
         )}
-        <form className={classes.search} onSubmit={(e) => e.preventDefault()}>
-          <label htmlFor="search" className={classes.label}>
-            <input
-              id="search"
-              type="text"
-              placeholder=""
-              value={search}
-              className={classes.input}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-          </label>
-        </form>
-        <form className={classes.search} onSubmit={(e) => e.preventDefault()}>
-          <label htmlFor="search" className={classes.label}>
-            Search by
-            <select
-              className={classes.option}
-              selected={searchFilter}
-              onChange={(e) => setSearchFilter(e.target.value)}
-            >
-              <option value="title">title</option>
-              <option value="author">author</option>
-              <option value="year">year</option>
-              <option value="publisher">publisher</option>
-              {roles !== 'Member' && <option value="serNo">serial no</option>}
-              {roles === 'Member' && <option value="category">category</option>}
-            </select>
-          </label>
-        </form>
-        <div style={{ marginLeft: '4rem' }}>
-          <form className={classes.search} onSubmit={(e) => e.preventDefault()}>
-            <label htmlFor="search" className={classes.label}>
-              Showing
-              <select
-                className={classes.option}
-                selected={roleFilter}
-                onChange={(e) => setRoleFilter(e.target.value)}
-              >
-                {categories.map((category) => (
-                  <option value={category} key={category}>
-                    {category}
-                  </option>
-                ))}
-              </select>
-            </label>
-          </form>
-        </div>
+        <FormControl variant="standard">
+          <InputLabel htmlFor="demo-customized-textbox">search users</InputLabel>
+          <BootstrapInput
+            id="demo-customized-textbox"
+            type="text"
+            placeholder=""
+            value={search}
+            label="search books"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <SearchIconWrapper>
+            <SearchIcon color="primary" />
+          </SearchIconWrapper>
+        </FormControl>
+        <Typography style={{ margin: '2rem 0.5rem' }}>by</Typography>
+        <FormControl variant="standard">
+          <InputLabel htmlFor="demo-customized-textbox"> </InputLabel>
+          <Select
+            value={searchFilter}
+            onChange={(e) => setSearchFilter(e.target.value)}
+            input={<BootstrapInput />}
+          >
+            <MenuItem value="title">title</MenuItem>
+            <MenuItem value="author">author</MenuItem>
+            <MenuItem value="year">year</MenuItem>
+            <MenuItem value="publisher">publisher</MenuItem>
+            {roles !== 'Member' && <MenuItem value="serNo">serial no</MenuItem>}
+            {roles === 'Member' && <MenuItem value="category">category</MenuItem>}
+          </Select>
+        </FormControl>
+        <Typography style={{ margin: '2rem 0.5rem 2rem 9rem' }}>showing</Typography>
+        <FormControl variant="standard">
+          <InputLabel htmlFor="demo-customized-textbox"> </InputLabel>
+          <Select
+            value={roleFilter}
+            onChange={(e) => setRoleFilter(e.target.value)}
+            input={<BootstrapInput />}
+          >
+            {categories.map((category) => (
+              <MenuItem value={category} key={category}>
+                {category}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       </div>
 
       <BookTable books={searchResults} onShowSnackbar={showSnackbar} history={history} />
