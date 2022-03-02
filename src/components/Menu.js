898fdd0edb3ada921f569ddbfde68fbcab62ee8a
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-03-02 21:38:44 +0100

    initial commit

---
+++ src/components/Menu.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Menu from '@material-ui/core/Menu';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Avatar from '@material-ui/core/Avatar';
+
+function BasicMenu(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const { userAvatar, onLogout, username } = props;
+
+  const history = useHistory();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const goToSettings = () => {
+    history.push(`/users/settings/${username}`);
+    handleClose();
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <Avatar src={userAvatar} />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar src={userAvatar} />
+          <Typography style={{ marginLeft: '1rem' }}>{username}</Typography>
+        </MenuItem>
+        <MenuItem onClick={goToSettings}>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText>Settings</ListItemText>
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={onLogout}>
+          <ListItemIcon>
+            <ExitToAppIcon />
+          </ListItemIcon>
+          <ListItemText>Log out</ListItemText>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+BasicMenu.propTypes = {
+  userAvatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  onLogout: PropTypes.func.isRequired,
+};
+
+export default BasicMenu;
