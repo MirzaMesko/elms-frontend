2d9c7e269d6fc6b889db8dbd6018627da97e7f8c
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-03-22 20:38:05 +0100

    add button for dismissing notifications

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
@@ -12,10 +13,18 @@ const useStyles = makeStyles(() => ({
     margin: '0 auto',
     borderBottom: '1px solid #CCC',
   },
+  button: {
+    padding: '0.1rem 1rem',
+  },
+  firstRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 }));
 
 const Notification = (props) => {
-  const { notification } = props;
+  const { notification, dismiss } = props;
   const classes = useStyles();
   return (
     <div
@@ -23,17 +32,28 @@ const Notification = (props) => {
       style={{ backgroundColor: notification.seen === 'false' ? '#FFF' : '#f5f5f5' }}
     >
       <Typography variant="h5">{notification.message}</Typography>
-      <Typography variant="subtitle1" style={{ color: '#AAA' }}>
-        {new Date(notification.timestamp).toDateString()},{' '}
-        {new Date(notification.timestamp).getUTCHours()}:
-        {new Date(notification.timestamp).getMinutes()}
-      </Typography>
+      <div className={classes.firstRow}>
+        <Typography variant="subtitle1" style={{ color: '#AAA' }}>
+          {new Date(notification.timestamp).toDateString()},{' '}
+          {new Date(notification.timestamp).getUTCHours()}:
+          {new Date(notification.timestamp).getMinutes()}
+        </Typography>
+        <Button
+          className={classes.button}
+          variant="outlined"
+          size="small"
+          onClick={() => dismiss(notification)}
+        >
+          Dismiss
+        </Button>
+      </div>
     </div>
   );
 };
 
 Notification.propTypes = {
-  notification: PropTypes.objectOf(PropTypes.shape({})).isRequired,
+  notification: PropTypes.objectOf(PropTypes.shape({}), PropTypes.string).isRequired,
+  dismiss: PropTypes.func.isRequired,
 };
 
 export default Notification;
