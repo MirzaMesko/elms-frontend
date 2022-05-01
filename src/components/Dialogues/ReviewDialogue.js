63e67705252cd26ca317819c375e4c91c6bb87e7
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-05-01 21:20:40 +0200

    enable editing users review

--- src/components/Dialogues/ReviewDialogue.js
+++ src/components/Dialogues/ReviewDialogue.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
 import Button from '@material-ui/core/Button';
@@ -9,7 +10,7 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import PropTypes from 'prop-types';
 
 function ReviewDialog(props) {
-  const { show, close, addReview } = props;
+  const { show, close, addReview, review, title, onEdit } = props;
   const [open, setOpen] = React.useState(show);
   const [text, setText] = React.useState();
   const [textWarning, setTextWarning] = React.useState('');
@@ -25,6 +26,9 @@ function ReviewDialog(props) {
 
   React.useEffect(() => {
     setOpen(show);
+    if (review) {
+      setText(review.review);
+    }
   }, [show]);
 
   const handleClose = () => {
@@ -35,15 +39,24 @@ function ReviewDialog(props) {
 
   const onAddReview = () => {
     if (!text) {
-      setTextWarning('This filed must not be empty');
+      setTextWarning('This field must not be empty');
       return;
     }
     addReview(text);
   };
 
+  const editReview = () => {
+    if (!text) {
+      setTextWarning('This field must not be empty');
+      return;
+    }
+    onEdit(review._id, text);
+    close();
+  };
+
   return (
     <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-      <DialogTitle id="form-dialog-title">Compose review</DialogTitle>
+      <DialogTitle id="form-dialog-title">{title}</DialogTitle>
       <DialogContent>
         <div style={{ display: 'flex', width: '30rem' }}>
           <TextField
@@ -57,7 +70,7 @@ function ReviewDialog(props) {
             error={textWarning.length > 2}
             helperText={textWarning}
             onBlur={() =>
-              !text.length ? setTextWarning('This field must not be empty') : setTextWarning('')
+              !text?.length ? setTextWarning('This field must not be empty') : setTextWarning('')
             }
             fullWidth
             onChange={handleTextChange}
@@ -68,18 +81,28 @@ function ReviewDialog(props) {
         <Button onClick={handleClose} color="primary">
           Cancel
         </Button>
-        <Button onClick={onAddReview} color="primary">
-          add
+        <Button onClick={title.includes('Compose') ? onAddReview : editReview} color="primary">
+          {title.includes('Compose') ? 'add' : 'save'}
         </Button>
       </DialogActions>
     </Dialog>
   );
 }
 
+ReviewDialog.defaultProps = {
+  review: {},
+  title: 'Compose review',
+  onEdit: () => null,
+  addReview: () => null,
+};
+
 ReviewDialog.propTypes = {
   show: PropTypes.bool.isRequired,
   close: PropTypes.func.isRequired,
-  addReview: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  addReview: PropTypes.func,
+  onEdit: PropTypes.func,
+  review: PropTypes.objectOf(PropTypes.string, PropTypes.objectOf(PropTypes.string)),
 };
 
 export default ReviewDialog;
