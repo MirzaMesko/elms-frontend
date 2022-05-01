ff2de5e046ffcc393f0672fbd33fb620fe671510
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-05-01 21:22:29 +0200

    add functionality for editing & deleting reviews

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -5,10 +5,17 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import Avatar from '@material-ui/core/Avatar';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Fade from '@material-ui/core/Fade';
 // import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 // import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import ReviewDialog from '../Dialogues/ReviewDialogue';
+import { LightTooltip } from '../Helpers/Tooltip';
+import * as helpers from '../Helpers/helpers';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,59 +44,77 @@ const useStyles = makeStyles((theme) => ({
   firstRow: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'start',
+    justifyContent: 'space-between',
     marginBottom: '0.3rem',
   },
 }));
 
-function stringToColor(string) {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = '#';
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.substr(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  return color;
-}
-
-function stringAvatar(name) {
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-    },
-    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
-  };
-}
-
 function ReviewCard(props) {
-  const { review, user } = props;
+  const { review, reviewAuthor, authUser, onDelete, onEdit } = props;
+  const [showReviewDialogue, setShowReviewDialogue] = React.useState(false);
   const classes = useStyles();
 
   return (
     <Card className={classes.root} variant="outlined">
+      <ReviewDialog
+        show={showReviewDialogue}
+        close={() => setShowReviewDialogue(false)}
+        review={review}
+        title="Edit review"
+        onEdit={onEdit}
+      />
       <CardContent>
         <div className={classes.firstRow}>
-          <Avatar
-            className={classes.small}
-            src={user.image}
-            alt={user.username}
-            {...stringAvatar(`${user.username} ${user.name}`)}
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+            }}
           >
-            {user.username.slice(0, 1)}
-          </Avatar>
-          <Typography variant="subtitle1" color="textSecondary">
-            {user.username}
-          </Typography>
+            <Avatar
+              className={classes.small}
+              src={reviewAuthor.image}
+              alt={reviewAuthor.username}
+              {...helpers.stringAvatar(`${reviewAuthor.username} ${reviewAuthor.name}`)}
+            >
+              {reviewAuthor.username.slice(0, 1)}
+            </Avatar>
+            <Typography variant="subtitle1" color="textSecondary">
+              {reviewAuthor.username}
+            </Typography>
+          </div>
+
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              marginTop: '-0.8rem',
+            }}
+          >
+            {authUser === reviewAuthor.username && (
+              <>
+                <LightTooltip
+                  TransitionComponent={Fade}
+                  TransitionProps={{ timeout: 600 }}
+                  title="Edit"
+                >
+                  <IconButton aria-label="edit">
+                    <EditIcon fontSize="small" onClick={() => setShowReviewDialogue(true)} />
+                  </IconButton>
+                </LightTooltip>
+
+                <LightTooltip
+                  TransitionComponent={Fade}
+                  TransitionProps={{ timeout: 600 }}
+                  title="Delete"
+                >
+                  <IconButton aria-label="edit">
+                    <DeleteIcon fontSize="small" onClick={() => onDelete(review._id)} />
+                  </IconButton>
+                </LightTooltip>
+              </>
+            )}
+          </div>
         </div>
 
         <Typography className={classes.title}>{review.review}</Typography>
@@ -105,13 +130,18 @@ function ReviewCard(props) {
 }
 
 ReviewCard.propTypes = {
-  review: PropTypes.objectOf(PropTypes.string).isRequired,
-  user: PropTypes.objectOf(PropTypes.string).isRequired,
+  review: PropTypes.objectOf(PropTypes.string, PropTypes.objectOf(PropTypes.string)).isRequired,
+  reviewAuthor: PropTypes.objectOf(PropTypes.string, PropTypes.objectOf(PropTypes.string))
+    .isRequired,
+  authUser: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onEdit: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state, props) => ({
   // eslint-disable-next-line no-underscore-dangle
-  user: state.users.users.filter((u) => u._id === props.review.userId)[0],
+  reviewAuthor: state.users.users.filter((u) => u._id === props.review.userId)[0],
+  authUser: state.users.authUser.username,
 });
 
 export default connect(mapStateToProps)(ReviewCard);
