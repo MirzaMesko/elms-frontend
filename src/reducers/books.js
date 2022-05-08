0dc318a5329b765c722716f8c267b367595fd1b8
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-05-08 12:37:14 +0200

    add case for RETRIEVE_BOOKS_PENDING action type

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,4 +1,8 @@
-import { RETRIEVE_BOOKS_SUCCESS, RETRIEVE_BOOKS_FAIL } from '../actions/books';
+import {
+  RETRIEVE_BOOKS_SUCCESS,
+  RETRIEVE_BOOKS_FAIL,
+  RETRIEVE_BOOKS_PENDING,
+} from '../actions/books';
 
 const initialState = {
   books: [],
@@ -6,15 +10,25 @@ const initialState = {
 
 const books = (state = initialState, action) => {
   switch (action.type) {
+    case RETRIEVE_BOOKS_PENDING:
+      return {
+        ...state,
+        loading: true,
+      };
     case RETRIEVE_BOOKS_SUCCESS:
       return {
         ...state,
         books: action.books,
+        loading: false,
       };
     case RETRIEVE_BOOKS_FAIL:
       return {
         ...state,
-        books: action.error,
+        error: {
+          error: true,
+          message: action.error,
+        },
+        loading: false,
       };
     default:
       return state;
