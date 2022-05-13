80e54753ca4a5674258bd09c9a2f62972e4bfd78
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-05-13 12:16:12 +0200

    add switch to keep entered input after adding book

--- src/components/Dialogues/BookDialogue.js
+++ src/components/Dialogues/BookDialogue.js
@@ -3,7 +3,7 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import Typography from '@material-ui/core/Typography';
@@ -12,95 +12,89 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import PropTypes from 'prop-types';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 import { addBook, getBooks, editBook } from '../../actions/books';
 import Confirm from '../Helpers/Confirm';
 import BasicSelect from '../Helpers/Select';
 import editionPlaceholder from '../../utils/edition_placeholder2.png';
 
 const categoryOptions = [
-  'All Books',
-  'Politics',
-  'History',
-  'Romance',
-  'Science Fiction & Fantasy',
-  'Biographies',
-  'Classics',
-  'Course books',
+  'All Books', 'Politics', 'History', 'Romance', 'Science Fiction & Fantasy', 'Biographies', 'Classics', 'Course books',
 ];
 
 function BookDialog(props) {
-  const { show, close, token, onGetBooks, onShowSnackbar, title, book, onEditBook, onAddBook, authUserRoles } = props;
+  const { show, close, token, onShowSnackbar, title, book, authUserRoles } = props;
 
-  const [open, setOpen] = React.useState(show);
-  const [openConfirm, setOpenConfirm] = React.useState(false);
-  const [bookId, setBookId] = React.useState('');
-  const [bookTitle, setBookTitle] = React.useState('');
-  const [author, setAuthor] = React.useState('');
-  const [year, setYear] = React.useState('');
-  const [image, setImage] = React.useState(editionPlaceholder);
-  const [description, setDescription] = React.useState('');
-  const [publisher, setPublisher] = React.useState('');
-  const [serNo, setSerNo] = React.useState('');
-  const [category, setCategory] = React.useState('');
+  const [state, setState] = React.useState({
+    bookId: '',
+    bookTitle: '',
+    author: '',
+    year: '',
+    image: editionPlaceholder,
+    description: '',
+    publisher: '',
+    serNo: '',
+    category: '',
+    keepInfo: false,
+    open: show,
+    openConfirm: false,
+  });
+
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
 
   const handleCategoryChange = (selectedCategory) => {
-    setCategory(selectedCategory);
-  };
-
-  const handleBookTitleChange = (event) => {
-    setBookTitle(event.target.value);
-  };
-
-  const handleAuthorChange = (event) => {
-    setAuthor(event.target.value);
-  };
-
-  const handleYearChange = (event) => {
-    setYear(event.target.value);
-  };
-
-  const handleDescriptionChange = (event) => {
-    setDescription(event.target.value);
-  };
-
-  const handlePublisherChange = (event) => {
-    setPublisher(event.target.value);
-  };
-
-  const handleSerialNumberChange = (event) => {
-    setSerNo(event.target.value);
+    setState({ ...state, category: selectedCategory });
   };
 
   const handleImageChange = (event) => {
-    setImage(event.target.value);
+    setState({ ...state, image: event.target.value });
+  };
+
+  const handleKeepInfoChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
   };
 
   const resetInput = () => {
-    setCategory('');
-    setBookTitle('');
-    setAuthor('');
-    setYear('');
-    setDescription('');
-    setPublisher('');
-    setSerNo('');
-    setBookId();
+    setState({
+      ...state,
+      bookId: '',
+      bookTitle: '',
+      author: '',
+      year: '',
+      image: editionPlaceholder,
+      description: '',
+      publisher: '',
+      serNo: '',
+      category: '',
+      keepInfo: false,
+      open: false,
+      openConfirm: false,
+    });
   };
 
   const handleClose = () => {
-    setOpen(false);
     close();
-    setOpenConfirm(false);
     resetInput();
+    dispatch(getBooks(token));
   };
 
   const onAddNewBook = (event) => {
     event.preventDefault();
-    if (!bookTitle || !author || !serNo || !year) {
+    if (!state.bookTitle || !state.author || !state.serNo || !state.year) {
       onShowSnackbar(true, 'error', 'Please fill in the required fileds!');
       return;
     }
-    onAddBook(authUserRoles, bookTitle, author, year, description, category, image, publisher, serNo, token)
-    .then((response) => {
+    dispatch(
+      addBook(
+        authUserRoles, state.bookTitle, state.author, state.year, state.description, state.category, state.image,
+        state.publisher, state.serNo,token
+      )
+    ).then((response) => {
       if (response.status !== 201) {
         onShowSnackbar(true, 'error', `${response.message}`);
       }
@@ -110,59 +104,68 @@ function BookDialog(props) {
           'success',
           `${response.data.title} by ${response.data.author} was added to the database.`
         );
-        onGetBooks(token);
-        close();
-        resetInput();
+        if (!state.keepInfo) {
+          handleClose();
+        }
       }
     });
   };
 
   const onEditSingleBook = (event) => {
     event.preventDefault();
-    onEditBook(authUserRoles, bookId, bookTitle, author, year, description, category, image, publisher, serNo, token)
-    .then((response) => {
+    dispatch(
+      editBook(
+        authUserRoles, state.bookId, state.bookTitle, state.author, state.year,
+        state.description, state.category, state.image, state.publisher, state.serNo,
+        token
+      )
+    ).then((response) => {
       if (response.status === 400 || response.status === 401 || response.status === 403) {
         onShowSnackbar(true, 'error', response.message);
       }
       if (response.status === 200) {
         onShowSnackbar(true, 'success', `Book ${response.data.serNo} was edited`);
-        onGetBooks(token);
-        close();
+        handleClose();
       }
     });
   };
 
   const showConfirm = () => {
-    if (bookTitle.length || author.length) {
-      setOpenConfirm(true);
+    if (state.bookTitle?.length || state.author?.length) {
+      setState({...state, openConfirm: true})
     } else {
       handleClose();
     }
   };
 
   React.useEffect(() => {
-    setOpen(show);
+    setState({ ...state, open: show});
     if (book.title) {
-      setBookId(book._id);
-      setBookTitle(book.title);
-      setAuthor(book.author);
-      setYear(book.year);
-      setDescription(book.description);
-      setPublisher(book.publisher);
-      setSerNo(book.serNo);
-      setCategory(book.category);
-      setImage(book.image || editionPlaceholder);
+      setState({
+        ...state,
+        bookId: book._id,
+        bookTitle: book.title,
+        author: book.author,
+        year: book.year,
+        image: book.image || editionPlaceholder,
+        description: book.description,
+        publisher: book.publisher,
+        serNo: book.serNo,
+        category: book.category,
+        keepInfo: false,
+        open: show
+      });
     }
   }, [show, book]);
 
   return (
-    <Dialog open={open} onClose={showConfirm} aria-labelledby="form-dialog-title">
+    <Dialog open={state.open} onClose={showConfirm} aria-labelledby="form-dialog-title">
       <Confirm
-        show={openConfirm}
+        show={state.openConfirm}
         title="Are you sure?"
         message="Entered input will be lost. Are you sure you want to cancel?"
         confirm={handleClose}
-        cancel={() => setOpenConfirm(false)}
+        cancel={() => setState({...state, openConfirm: false})}
         cancelText="confirm cancel"
         confirmText="continue editing"
       />
@@ -175,7 +178,7 @@ function BookDialog(props) {
               type="image"
               id="image"
               alt="Login"
-              src={image}
+              src={state.image}
               style={{
                 height: '320px',
                 display: 'block',
@@ -189,33 +192,36 @@ function BookDialog(props) {
               margin="dense"
               id="title"
               label="Title*"
-              defaultValue={bookTitle}
+              defaultValue={state.bookTitle}
               type="text"
               fullWidth
-              onChange={handleBookTitleChange}
+              name="bookTitle"
+              onChange={handleChange}
             />
             <TextField
               focus="true"
               margin="dense"
               id="author"
-              defaultValue={author}
+              defaultValue={state.author}
               label="Author*"
               type="text"
               fullWidth
-              onChange={handleAuthorChange}
+              name="author"
+              onChange={handleChange}
             />
             <TextField
               margin="dense"
               id="year"
               label="Year*"
               type="year"
-              defaultValue={year}
+              defaultValue={state.year}
               fullWidth
-              onChange={handleYearChange}
+              name="year"
+              onChange={handleChange}
             />
             <BasicSelect
               onChange={handleCategoryChange}
-              selected={category || 'All books'}
+              selected={state.category || 'All books'}
               options={categoryOptions}
               label="Category"
             />
@@ -224,18 +230,20 @@ function BookDialog(props) {
               id="publisher"
               label="Publisher"
               type="text"
-              defaultValue={publisher}
+              defaultValue={state.publisher}
               fullWidth
-              onChange={handlePublisherChange}
+              name="publisher"
+              onChange={handleChange}
             />
             <TextField
               margin="dense"
               id="serNo"
               label="Serial No*"
               type="text"
-              defaultValue={serNo}
+              defaultValue={state.serNo}
               fullWidth
-              onChange={handleSerialNumberChange}
+              name="serNo"
+              onChange={handleChange}
             />
           </div>
         </div>
@@ -244,9 +252,10 @@ function BookDialog(props) {
           id="image"
           label="Image Url"
           type="text"
-          defaultValue={image}
+          defaultValue={state.image}
           fullWidth
-          onChange={handleImageChange}
+          name="image"
+          onChange={handleChange}
         />
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <Typography>or</Typography>
@@ -271,10 +280,24 @@ function BookDialog(props) {
           multiline
           label="Description"
           type="text"
-          defaultValue={description}
+          defaultValue={state.description}
           fullWidth
-          onChange={handleDescriptionChange}
+          name="description"
+          onChange={handleChange}
         />
+        {title.includes('Add') && (
+          <FormControlLabel
+            control={
+              <Switch
+                checked={state.keepInfo}
+                onChange={handleKeepInfoChange}
+                name="keepInfo"
+                color="primary"
+              />
+            }
+            label="save entered input for another entry"
+          />
+        )}
       </DialogContent>
       <DialogActions>
         <Button onClick={showConfirm} color="primary">
@@ -292,7 +315,6 @@ BookDialog.propTypes = {
   show: PropTypes.bool.isRequired,
   close: PropTypes.func.isRequired,
   token: PropTypes.string.isRequired,
-  onGetBooks: PropTypes.func.isRequired,
   onShowSnackbar: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
   book: PropTypes.objectOf(
@@ -303,8 +325,6 @@ BookDialog.propTypes = {
       PropTypes.objectOf({}),
     ])
   ),
-  onEditBook: PropTypes.func.isRequired,
-  onAddBook: PropTypes.func.isRequired,
   authUserRoles: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
@@ -317,12 +337,4 @@ const mapStateToProps = (state) => ({
   authUserRoles: state.users.authUser.roles,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  onEditBook: (authUserRoles, bookId, title, author, year, description, category, image, publisher, serNo, token) => 
-    dispatch(editBook(authUserRoles, bookId, title, author, year, description, category, image, publisher, serNo, token)),
-  onGetBooks: (token) => dispatch(getBooks(token)),
-  onAddBook: (authUserRoles, title, author, year, description, category, image, publisher, serNo, token) => 
-    dispatch(addBook(authUserRoles, title, author, year, description, category, image, publisher, serNo, token)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(BookDialog);
+export default connect(mapStateToProps)(BookDialog);
