42181a445ef91257dcc47ba2048fa50b6578e0e5
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-08-27 19:38:22 +0200

    initial commit

---
+++ src/components/Helpers/Chip.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
+import Chip from '@material-ui/core/Chip';
+import * as helpers from './helpers';
+// @ts-ignore
+import type { User } from '../../types.ts';
+
+interface Props {
+  user: User;
+}
+
+const RoleChip: React.FC<Props> = ({ user }: Props) =>
+  user.roles &&
+  Object.values(user.roles).map((item: any) => (
+    <Chip
+      key={item + user._id}
+      icon={helpers.setIcon(item)}
+      size="small"
+      label={item}
+      color={helpers.roleColor(item)}
+      style={{ margin: '3px' }}
+    />
+  ));
+
+export default RoleChip;
