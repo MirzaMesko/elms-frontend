617d7a11c0bd820cfd5f7b002611f1e69b4fccc1
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-09-11 19:47:44 +0200

    initial commit after moving dialog from Book.tsx

---
+++ src/components/Dialogues/RatingDialogue.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+// @ts-ignore
+import Ratings from '../Helpers/Rating.tsx';
+
+interface Props {
+  open: boolean;
+  close: () => void;
+  rate: () => void;
+}
+
+const RatingDialogue: React.FC<Props> = ({ open, close, rate }: Props) => (
+  <Dialog open={open} onClose={close}>
+    <DialogContent style={{ padding: '1rem 3rem' }}>
+      <Ratings name="hover-feedback" onClick={rate} />
+    </DialogContent>
+  </Dialog>
+);
+
+export default RatingDialogue;
