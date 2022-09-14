4ad41e419619236929f0a86f5da3e300b2ee52af
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-09-14 22:07:21 +0200

    initial commit

---
+++ src/components/__test__/BookTable.test.js
@@ -0,0 +1,94 @@
+import { screen, render, cleanup } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as reactRedux from 'react-redux';
+import { Provider } from 'react-redux';
+import { store } from '../../store.ts';
+import BookTable from '../Book/BookTable.tsx';
+import '@testing-library/jest-dom/extend-expect';
+
+const booksArray = [
+  {
+    _id: '123456789',
+    title: 'title',
+    author: 'author',
+    image: 'image',
+    year: '1988',
+    category: 'classics',
+    description: `Lorem ipsum dolor sit amet`,
+  },
+  {
+    _id: '987654321',
+    title: 'title',
+    author: 'author',
+    image: 'image',
+    year: '1988',
+    category: 'classics',
+    description: `Lorem ipsum dolor sit amet`,
+  },
+];
+
+const setup = (books) => {
+  render(
+    <Provider store={store}>
+      <BookTable books={books} />
+    </Provider>
+  );
+};
+
+afterEach(() => cleanup());
+
+describe('BookTable', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Provider store={store}>
+        <BookTable books={[]} />
+      </Provider>,
+      div
+    );
+  });
+  it(`renders 'Books' as title of table`, () => {
+    setup([]);
+
+    expect(screen.getByTestId('book-table-title')).toBeInTheDocument();
+  });
+  it(`renders 'No matches for your search' message if search returns no results`, () => {
+    setup([]);
+
+    expect(screen.getByTestId('no-results-message')).toBeInTheDocument();
+    expect(screen.getByTestId('no-results-message').textContent).toBe('No matches for your search');
+  });
+  it('renders table with tablehead, tablebody and pagination', () => {
+    setup(booksArray);
+
+    expect(screen.queryByTestId('table-head')).toBeInTheDocument();
+    expect(screen.queryByTestId('book-table-body')).toBeInTheDocument();
+    expect(screen.queryByTestId('table-pagination')).toBeInTheDocument();
+  });
+  it('renders Loading component if waiting for API response', () => {
+    const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
+    useSelectorMock.mockReturnValue({ loading: true });
+    render(
+      <reactRedux.Provider store={store}>
+        <BookTable books={[]} />
+      </reactRedux.Provider>
+    );
+
+    expect(screen.getByTestId('loading')).toBeInTheDocument();
+    useSelectorMock.mockClear();
+  });
+  it('renders Error component if error after API request', () => {
+    const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
+    useSelectorMock.mockReturnValue({ error: { error: true, message: 'test' } });
+    render(
+      <reactRedux.Provider store={store}>
+        <BookTable books={[]} />
+      </reactRedux.Provider>
+    );
+
+    expect(screen.getByTestId('error')).toBeInTheDocument();
+    expect(screen.getByTestId('error-message').textContent).toBe('test');
+    useSelectorMock.mockClear();
+  });
+});
