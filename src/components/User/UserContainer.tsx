74287c2811164e016531cb8d5d897f48def141d1
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-09-26 21:40:39 +0200

    remove CustomizedSnackbars

--- src/components/User/UserContainer.tsx
+++ src/components/User/UserContainer.tsx
@@ -5,8 +5,6 @@ import { useDispatch, useSelector } from 'react-redux';
 // @ts-ignore
 import { notifyUser } from '../../actions/users.tsx';
 // @ts-ignore
-import CustomizedSnackbars from '../Helpers/Snackbar.tsx';
-// @ts-ignore
 import EmailDialogue from '../Dialogues/EmailDialogue.tsx';
 // @ts-ignore
 import NotificationDialogue from '../Dialogues/NotificationDialogue.tsx';
@@ -18,6 +16,8 @@ import { RootState, AppDispatch } from '../../store.ts';
 import type { User } from '../../types.ts';
 // @ts-ignore
 import UserDetails from './User.tsx';
+// @ts-ignore
+import { showSnackbarMessage } from '../../actions/books.tsx';
 
 interface OwnProps {
   open: boolean;
@@ -30,50 +30,32 @@ type Props = OwnProps & RootState & AppDispatch;
 const UserContainer: React.FC<OwnProps> = ({ open, handleClose, user }: Props) => {
   const [showEmailDialogue, setShowEmailDialogue] = React.useState(false);
   const [showNotificationDialogue, setShowNotificationDialogue] = React.useState(false);
-  const [severity, setSeverity] = React.useState('');
-  const [openSnackbar, setOpenSnackbar] = React.useState(false);
-  const [errMessage, setErrMessage] = React.useState('');
 
   const dispatch: AppDispatch = useDispatch();
   const history: any = useHistory();
   const { token, authUser } = useSelector((state: RootState) => state.users);
 
-  const showSnackbar = (show: boolean, status: string, message: string) => {
-    setSeverity(status);
-    setErrMessage(message);
-    setOpenSnackbar(show);
-    setTimeout(() => {
-      setOpenSnackbar(false);
-    }, 6000);
-  };
-
   const handleSendEmail = (email: string, subject: string, text: string) => {
     dispatch(sendEmail(token, authUser.roles, email, subject, text)).then((resp: any) => {
-      if (resp.status !== 200) {
-        showSnackbar(true, 'error', resp.message);
-      }
       if (resp.status === 200) {
         setShowEmailDialogue(false);
-        showSnackbar(true, 'success', `An email  was sent to ${user.username}.`);
       }
     });
   };
 
   const handleSendNotification = (text: string) => {
     dispatch(notifyUser(token, authUser.roles, user._id, text)).then((resp: any) => {
-      if (resp.status !== 200) {
-        showSnackbar(true, 'error', resp.message);
-      }
       if (resp.status === 200) {
         setShowNotificationDialogue(false);
-        showSnackbar(true, 'success', `Notification was sent to ${user.username}.`);
+        dispatch(
+          showSnackbarMessage('success', `Your notification has been sent to ${user.username}.`)
+        );
       }
     });
   };
 
   return (
     <>
-      <CustomizedSnackbars show={openSnackbar} severity={severity} message={errMessage} />
       <EmailDialogue
         show={showEmailDialogue}
         close={() => setShowEmailDialogue(false)}
