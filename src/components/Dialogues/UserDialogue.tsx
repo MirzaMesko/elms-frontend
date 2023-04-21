a8520f4ff334bb92f1617f9b38fab7cb250cd520
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2023-04-21 19:28:14 +0200

    update image upload & css values for small screens

--- src/components/Dialogues/UserDialogue.tsx
+++ src/components/Dialogues/UserDialogue.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
+import ImageUploading, { ImageListType } from 'react-images-uploading';
 import Button from '@material-ui/core/Button';
-import { connect, useDispatch } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import IconButton from '@material-ui/core/IconButton';
+import { useDispatch, useSelector } from 'react-redux';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
-import Typography from '@material-ui/core/Typography';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
@@ -22,6 +26,27 @@ import { RootState, AppDispatch } from '../../store.ts';
 // @ts-ignore
 import type { User } from '../../types.ts';
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    [theme.breakpoints.down('xs')]: {
+      maxWidth: '90vw',
+    },
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    marginTop: '-4.5rem',
+  },
+  cameraIcon: {
+    color: '#f5f5f5',
+    backgroundColor: 'rgb(0, 0, 0, 0.2)',
+    padding: '5px',
+    fontSize: '48',
+    borderRadius: '20%',
+  },
+}));
+
 interface OwnProps {
   show: boolean;
   close: () => void;
@@ -37,9 +62,11 @@ const roleOptions = ['Admin', 'Librarian', 'Member'];
 
 type Props = OwnProps & RootState;
 
-const UserDialog: React.FC<OwnProps> = (props: Props) => {
-  const { show, close, token, onShowSnackbar, user, title, authUserRoles, authUser } = props;
+const UserDialog: React.FC<OwnProps> = ({ show, close, onShowSnackbar, user, title }: Props) => {
+  const maxNumber = 1;
+  const classes = useStyles();
 
+  const [images, setImages] = React.useState<any>([]);
   const [state, setState] = React.useState({
     userId: '',
     username: '',
@@ -54,6 +81,8 @@ const UserDialog: React.FC<OwnProps> = (props: Props) => {
   });
 
   const dispatch: AppDispatch = useDispatch();
+  const { token, authUser } = useSelector((reduxState: RootState) => reduxState.users);
+  const { roles } = authUser;
 
   const handleChange = (event: { target: { name: string; value: string | boolean } }) => {
     setState({ ...state, [event.target.name]: event.target.value });
@@ -61,6 +90,16 @@ const UserDialog: React.FC<OwnProps> = (props: Props) => {
 
   const handleRoleChange = (selectedRoles: Array<string>) => {
     setState({ ...state, roles: selectedRoles });
+  };
+
+  const onChange = (imageList: ImageListType, addUpdateIndex: any) => {
+    // data for submit
+    // eslint-disable-next-line no-console
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+    // const file = event.target.files[0];
+    // eslint-disable-next-line no-console
+    setState({ ...state, image: imageList[0].dataURL });
   };
 
   const resetInput = () => {
@@ -77,6 +116,7 @@ const UserDialog: React.FC<OwnProps> = (props: Props) => {
       open: false,
       openConfirm: false,
     });
+    setImages([]);
   };
 
   const handleClose = () => {
@@ -93,7 +133,7 @@ const UserDialog: React.FC<OwnProps> = (props: Props) => {
     }
     dispatch(
       addUser(
-        authUserRoles,
+        roles,
         state.email,
         state.username,
         state.password,
@@ -112,12 +152,10 @@ const UserDialog: React.FC<OwnProps> = (props: Props) => {
 
   const onEditSingleUser = (event: { preventDefault: () => void }) => {
     event.preventDefault();
-    if (authUser === state.username) {
-      authUserRoles.push('Admin');
-    }
+    const userRoles = authUser.username === state.username ? ['Admin'] : roles;
     dispatch(
       editUser(
-        authUserRoles,
+        userRoles,
         state.userId,
         state.email,
         state.roles,
@@ -131,9 +169,6 @@ const UserDialog: React.FC<OwnProps> = (props: Props) => {
         handleClose();
       }
     });
-    if (authUser === state.username) {
-      authUserRoles.pop();
-    }
   };
 
   const showConfirm = () => {
@@ -168,6 +203,7 @@ const UserDialog: React.FC<OwnProps> = (props: Props) => {
       onClose={showConfirm}
       aria-labelledby="form-dialog-title"
       data-testid="user-dialogue"
+      className={classes.container}
     >
       <Confirm
         show={state.openConfirm}
@@ -184,6 +220,17 @@ const UserDialog: React.FC<OwnProps> = (props: Props) => {
         <div className="dialogueContainer">
           <div>
             <input type="image" id="image" alt="Login" src={state.image} className="mediumImage" />
+            <div className={classes.content}>
+              <ImageUploading multiple value={images} onChange={onChange} maxNumber={maxNumber}>
+                {({ onImageUpload, dragProps }) => (
+                  <div>
+                    <IconButton onClick={onImageUpload} {...dragProps}>
+                      <CameraAltIcon className={classes.cameraIcon} />
+                    </IconButton>
+                  </div>
+                )}
+              </ImageUploading>
+            </div>
           </div>
           <div style={{ marginLeft: '1rem' }}>
             <TextField
@@ -211,7 +258,7 @@ const UserDialog: React.FC<OwnProps> = (props: Props) => {
             <MulitpleSelect
               onChange={handleRoleChange}
               selected={state.roles}
-              disabled={!authUserRoles.includes('Admin')}
+              disabled={!roles.includes('Admin')}
               options={roleOptions}
               label="Roles"
             />
@@ -236,34 +283,6 @@ const UserDialog: React.FC<OwnProps> = (props: Props) => {
               onChange={handleChange}
             />
           </div>
-        </div>
-        <TextField
-          margin="dense"
-          id="image"
-          label="Image Url"
-          type="text"
-          defaultValue={state.image}
-          fullWidth
-          name="image"
-          onChange={handleChange}
-        />
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <Typography>or</Typography>
-          <label htmlFor="raised-button-file">
-            <input
-              accept="image/*"
-              style={{ display: 'none' }}
-              id="raised-button-file"
-              multiple
-              type="file"
-              hidden
-              name="image"
-              onChange={handleChange}
-            />
-            <Button variant="outlined" component="span">
-              Upload image
-            </Button>
-          </label>
         </div>
 
         <TextField
@@ -290,10 +309,6 @@ const UserDialog: React.FC<OwnProps> = (props: Props) => {
   );
 };
 
-const mapStateToProps = (state: RootState) => ({
-  token: state.users.token,
-  authUserRoles: state.users.authUser.roles,
-  authUser: state.users.authUser.username,
-});
+export default UserDialog;
 
-export default connect<RootState, OwnProps>(mapStateToProps)(UserDialog);
+
